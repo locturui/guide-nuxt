036cc90a5a4f92b6c360041189e5633fe3dcde5a
Stepan Leonov <stepan.leonov777@gmail.com>
2025-05-20 16:27:43 +0300

    eslint config / husky

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+    optimizeDeps: {
+      include: [
+        "maplibre-gl",
+      ],
+    },
+  },
+});
