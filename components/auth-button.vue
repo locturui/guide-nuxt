1217803e777819cb2943079e56d2fa4be82a5bc0
Stepan Leonov <stepan.leonov777@gmail.com>
2025-07-14 15:24:49 +0300

    revamp

---
+++ components/auth-button.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+// const authStore = useAuthStore();
+</script>
+
+<template>
+  <!-- <div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <div
+      tabindex="0"
+      role="button"
+      class="btn m-1"
+    >
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon
+            name="tabler:logout-2"
+            size="20"
+          />
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+  <button
+    v-else
+    :disabled="authStore.loading"
+    class="btn btn-accent"
+    @click="authStore.signIn"
+  >
+    Sign in with GitHub
+    <span v-if="authStore.loading" class="loading loading-dots loading-sm" />
+    <Icon
+      v-else
+      name="tabler:brand-github"
+      size="20"
+    />
+  </button> -->
+  <div />
+</template>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+// const authStore = useAuthStore();
+</script>
+
+<template>
+  <div class="hero bg-base-100 container mx-auto mt-5">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Бронирование слотов
+        </h1>
+        <p class="py-6">
+          Это приложение позволяет бронировать слоты для экскурсий. Вы можете выбрать дату, время и количество гостей.
+          Для начала работы, пожалуйста, войдите в систему.
+        </p>
+        <!-- <AuthButton v-if="!authStore.user" /> -->
+        <NuxtLink
+          to="/bookings"
+          class="btn btn-primary"
+        >
+          К расписанию
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
